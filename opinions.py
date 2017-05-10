0f9345fd3ec2e82b3b4658923ec1060ef752ac65
Monte Roden <rodenmonte@gmail.com>
2017-05-09 22:57:45 -0700

    Added potential functions

--- opinions.py
+++ opinions.py
@@ -4,44 +4,37 @@ from mesa.time import SimultaneousActivation
 from mesa.datacollection import DataCollector
 import numpy as np
 import random
+import potentials
 
-ALPHA = .01 #TEMPORARY, in the future ALPHA should be able to be passed by the user & varied. Recall alpha measures learning rate.
-def potential_prime(diff):
-    '''
-    Tent function potential, tip at .5
-    '''
-    if(diff < .5):
-        return 1
-    elif(diff > .5):
-        return -1
-    else:
-        return 0
-
+ALPHA = .001 #TEMPORARY, in the future ALPHA should be able to be passed by the user & varied. Recall alpha measures learning rate. Maybe make constants.py to hold just ALPHA?
 
 class OpinionAgent(Agent):
     '''
     An agent has an opinion, and is inlfuenced by other agent's opinions based off a potential energy function. For now, initial opinion of an agent is random
     '''
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, neighbors, potential):
         super().__init__(unique_id, model) # sets self.model
         self.opinion = np.random.rand() #From 0 to 1
         self.nextOpinion = None
-    
+        self.neighbors = neighbors # neighbors are the people an agent is influenced by. As a viewer, I may consider a news channel a "neighbor" of sorts, but they would probably not think of me at all.
+        self.potential = potential
+         
     def step(self):
         '''
         Calculates next opinion based off of all other agent's opinions. Models a totally connected graph.
-        o_i^(t+1) = o_i^t + sum(j = 1 to k, (- alpha / 2) * potential'(|diff|) * diff/ |diff| is the formula for the  next opinion from the paper. 
+        o_i^(t+1) = o_i^t + sum(j = 1 to k, (- alpha / 2) * potential'(|diff|) * diff/ |diff| is the formula for the  next opinion from the paper. 1 to k represents all neighbors. 
         '''
         # self.model.schedule.agents is a list containing all other agents.
         self.nextOpinion = self.opinion
         
         # This calculates the next opinion based off the formula
-        for other in self.model.schedule.agents[:]:
-            difference = self.opinion - other.opinion
+        for other in self.neighbors: #Other is just a number.
+            difference = self.opinion - self.model.schedule.agents[other].opinion
             if difference == 0: #Don't calculate for the same node. Unsure what to do if there are other nodes of the same opinion, probably doesn't matter at this point
                 pass;
             else:
-                self.nextOpinion += ((-1) * ALPHA / 2) * potential_prime(abs(difference)) * (difference / abs(difference))
+                #The negative is in the potential function now.
+                self.nextOpinion += (ALPHA / 2) * self.potential(abs(difference)) * (difference / abs(difference))
 
         # Clamp function
         if self.nextOpinion > 1:
@@ -61,11 +54,15 @@ class OpinionModel(Model):
         self.schedule = SimultaneousActivation(self)
         #Create agents
         for i in range(self.num_agents):
-            a = OpinionAgent(i, self)
+            #neighbors = [random.randint(0, N-1) for i in range((N + 3) // 4)] #So that nodes don't have 0 neighbors...
+            neighbors = [i for i in range(N)]
+            a = OpinionAgent(i, self, neighbors, potentials.gaussian())
             self.schedule.add(a)
+
         self.datacollector = DataCollector(
                 agent_reporters = {"Opinion": lambda a: a.opinion}
         )
+        
     def step(self):
         '''Advance the model one step'''
         self.datacollector.collect(self)
---
+++ potentials.py
@@ -0,0 +1,37 @@
+'''
+The functions below are the negatives of the derivatives they represent in the paper.
+Note that these functions return functions, not values. TODO tell the user where these functions can be found in the paper. Perhaps implement other potential functions?
+'''
+from math import e
+from math import pow
+from math import sqrt
+from math import pi
+def tent(tau_l=.5, tau_u=.5): 
+    def impl(diff):
+        if(diff < tau_l): #Left half of the potential function.
+            return -1 * (1 / tau_l) #Rise over run, times -1, because its a potential function.
+        elif(diff > tau_u):
+            return 1 * (1 / (1 - tau_u)) #the x distance between the end of y=1 and y=0 is 1 - tau_u.
+        else:
+            return 0
+    return impl
+
+def bcm(tau):
+    def impl(diff):
+        if(diff < tau):
+            return 2 * diff
+        else:
+            return 0
+    return impl
+
+def gaussian(mean=.5, stddev=.5):
+    #Recall the gaussian is: (1 / (u * sqrt( 2 * pi)) * e ^ (- x^2 / (2 * u^2)), and we care about its derivative in terms of x (the mean), which gives its graphical slope. 
+    def impl(diff):
+        return 1 * (diff - mean) / (pow(stddev, 3) * sqrt(2 * pi)) * pow(e, -1 * pow(diff - mean, 2) / (2 * pow(stddev, 2)))
+    return impl
+
+def simple():
+    def impl(diff=0): #Default parameter here, because it has no effect. If someone decides not to use a parameter in this case, it won't break their code. A function is returned only to maintain form with the other potential functions
+        return -1
+    return impl
+
